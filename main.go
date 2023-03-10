7cfe57869bc0f7ed6bc34a689fd8516d8777c289
NoNamePL <rdymshakov@inbox.ru>
2023-03-10 06:25:34 +0000

    refactor button on main page, add empty login and register html and css files, try to do PopUp window for login and register, if i can't then i will use separate page for login and register

--- main.go
+++ main.go
@@ -14,36 +14,50 @@ type Welcome struct {
 }
 
 func main() {
-	welcome := Welcome{"Anonymous", time.Now().Format(time.Stamp)}
-	r := gin.Default()
 
-	r.LoadHTMLFiles("templates/awesomeProject.html", "templates/index.html")
+	r := gin.Default()
+	//r.LoadHTMLFiles("templates/awesomeProject.html", "templates/index.html")
+	r.LoadHTMLGlob("templates/*.html")
 
 	r.Static("/static", "./static/")
 
-	r.GET("/ping", func(c *gin.Context) {
-
-		if name := c.Request.FormValue("name"); name != "" {
-			welcome.Name = name
-		}
-		c.HTML(
-			http.StatusOK,
-			"awesomeProject.html",
-			gin.H{
-				"status": http.StatusOK,
-				"Name":   welcome.Name,
-				"Time":   welcome.Time,
-			},
-		)
-	})
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(
-			http.StatusOK,
-			"index.html",
-			gin.H{
-				"status": http.StatusOK,
-			},
-		)
-	})
+	r.GET("/ping", handlerPing)
+	r.GET("/", handlerMain)
+	r.GET("/login", handlerLogin)
+	r.GET("/register", handlerRegister)
 	log.Fatal(r.Run()) // listen and server on 0.0.0.0:8080
+}
+
+func handlerLogin(c *gin.Context) {
+
+}
+
+func handlerRegister(c *gin.Context) {
+
+}
+
+func handlerPing(c *gin.Context) {
+	welcome := Welcome{"Anonymous", time.Now().Format(time.Stamp)}
+	if name := c.Request.FormValue("name"); name != "" {
+		welcome.Name = name
+	}
+	c.HTML(
+		http.StatusOK,
+		"awesomeProject.html",
+		gin.H{
+			"status": http.StatusOK,
+			"Name":   welcome.Name,
+			"Time":   welcome.Time,
+		},
+	)
+}
+
+func handlerMain(c *gin.Context) {
+	c.HTML(
+		http.StatusOK,
+		"index.html",
+		gin.H{
+			"status": http.StatusOK,
+		},
+	)
 }
